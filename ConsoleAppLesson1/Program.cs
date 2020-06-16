af0332c4c435f45d819964b633075cca9734c4e1
dhurt <dhurt@DESKTOP-U6JE5R4>
2020-06-16 09:01:43 -0500

    Commit test

---
+++ ConsoleAppLesson1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleAppLesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Working with GIT Hub");
+
+        }
+    }
+}
